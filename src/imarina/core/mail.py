c6568b7c95292dc7610bf458483285a13286ea27
Mario Piqué Martinez <mpique@iciq.es>
2026-05-04 11:16:52 +0200

    delete comments in code

--- src/imarina/core/mail.py
+++ src/imarina/core/mail.py
@@ -30,27 +30,6 @@ def get_creator_email(token, site_id, list_id, item_id):
 
 
 
-# def send_mail2(username, password, to, host, port, subject, content):
-#     SMTP_SERVER = host
-#     SMTP_PORT = port
-#     USERNAME = username
-#     PASSWORD = password
-#
-#     msg = EmailMessage()
-#     msg["Subject"] = subject
-#     msg["From"] = USERNAME
-#     msg["To"] = to
-#     msg.set_content(content)
-#
-#     with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as smtp:
-#         smtp.starttls()  # Use TLS
-#         smtp.login(USERNAME, PASSWORD)
-#         smtp.send_message(msg)
-#
-#     print("Email sent successfully!")
-
-
-
 def send_email(to_email, subject, body, from_email, username, password, server: str, port: int):
     # Create message
     msg = MIMEText(body)
@@ -132,22 +111,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
 
-    # print("Sending email with another func")
-    # print("To: " + args.author)
-    # print("username: " + read_secret("SMTP_USERNAME"))
-    # print("password: " + read_secret("SMTP_PASSWORD"))
-    # print("port: " + read_secret("SMTP_PORT"))
-    # print("Server: " + read_secret("SMTP_SERVER"))
-    # send_mail(
-    #     to_email=args.author,
-    #     subject="test email",
-    #     body="a test of email",
-    #     from_email=read_secret("SMTP_USERNAME"),
-    #     username=read_secret("SMTP_USERNAME"),
-    #     password=read_secret("SMTP_PASSWORD"),
-    #     server=read_secret("SMTP_SERVER"),
-    #     port=read_secret("SMTP_PORT")
-    # )
 
     mail_process(args)
 
