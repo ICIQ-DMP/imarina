edf5c4ed8d00778e025bb5ebc6752774100e8574
AleixMT <aleixaretra@gmail.com>
2026-08-25 18:55:40 +0200

    feat: changed the input directory for upload command into ./output

--- src/imarina_load_researchers/commands/upload/cli.py
+++ src/imarina_load_researchers/commands/upload/cli.py
@@ -20,7 +20,7 @@ import typer
 from imarina_load_researchers.core.defines import (
     DEFAULT_PUBLISHED_FILE_PATH,
     DEFAULT_TARGET_DIR,
-    SHAREPOINT_LOCAL_OUTPUT_DIR,
+    LOCAL_OUTPUT_DIR,
 )
 from imarina_load_researchers.core.file_select import select_file_to_upload
 from imarina_load_researchers.core.log_utils import get_logger
@@ -54,7 +54,7 @@ def upload_controller(
     logger.info("Uploading the latest Excel file to SharePoint...")
 
     if file_path is None:
-        file_path = select_file_to_upload(SHAREPOINT_LOCAL_OUTPUT_DIR)
+        file_path = select_file_to_upload(LOCAL_OUTPUT_DIR)
 
     logger.info(f"Local file detected: {file_path.name}")
     logger.info(f"Destination SharePoint: {target_folder}")
